e6eab5398581cdfd04d3f4796c2582d85e4e1aac
alanvic4 <edsonsilva.vendas123@gmail.com>
2022-10-31 22:28:11 -0300

    Adding tippercentages and modify the body function

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -11,10 +11,12 @@ struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
     @State private var tipPercentage = 20
+    let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Section {
+                TextField("Amount", text: $checkAmount)
+            }
     }
 }
 
